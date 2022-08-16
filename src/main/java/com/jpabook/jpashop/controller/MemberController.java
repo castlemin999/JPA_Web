5b29e0b86c4d91f6944bba93cf18a23386df188e
castlemin999 <castlemin999@gmail.com>
2022-08-17 00:06:29 +0900

    feat : JPA Web[proceeding]

--- src/main/java/com/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.jpabook.jpashop.controller;
 
+import com.jpabook.jpashop.domain.Address;
+import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +19,15 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm",new MemberForm());
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String saveMember(MemberForm form) {
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+        return "redirect:/";
     }
 }
